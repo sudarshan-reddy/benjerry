8aeedc28736ebb39793e29544d4894294c08edc2
Sudarshan <sudar.theone@gmail.com>
2018-04-28 07:45:07 +0530

    added test for getIceCream

--- handlers/icecream_test.go
+++ handlers/icecream_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi"
 	"github.com/stretchr/testify/assert"
 	"github.com/sudarshan-reddy/benjerry/models"
 )
@@ -153,4 +154,71 @@ func Test_PostIceCreamData(t *testing.T) {
 			assert.Equal(test.expectedDataStore, iceCreamStore.serializedStore)
 		})
 	}
+}
+
+func Test_GetIceCreamData(t *testing.T) {
+	var tests = []struct {
+		desc               string
+		dbResponse         *models.IceCream
+		dbError            error
+		expectedResponse   string
+		expectedStatusCode int
+	}{
+		{
+			desc: "if the database returns a successful response, show that in response to get",
+			dbResponse: &models.IceCream{
+				Name:        "chocobar",
+				ImageOpen:   "imageOpen",
+				ImageClosed: "imageClose",
+				Story:       "some story",
+				Description: "some description",
+			},
+			expectedResponse: "{\"name\":\"chocobar\",\"image_open\":\"imageOpen\"," +
+				"\"image_closed\":\"imageClose\",\"story\":\"some story\"," +
+				"\"description\":\"some description\",\"sourcing_values\":null," +
+				"\"ingredients\":null,\"allergy_info\":\"\"," +
+				"\"dietary_certification\":\"\",\"product_id\":\"\"}\n",
+			expectedStatusCode: 200,
+		},
+		{
+			desc:    "if database returns ErrNoRows, return 404 in response",
+			dbError: models.ErrNoRows,
+			expectedResponse: "{\"httpStatus\":404,\"httpCode\":\"not_found\"," +
+				"\"requestId\":\"\",\"errors\":[{\"code\":\"not_found\"," +
+				"\"message\":\"Icecream: chocobar Not Found\"}]}\n",
+			expectedStatusCode: 404,
+		},
+		{
+			desc:               "if database returns a different error, return 500 in response",
+			dbError:            errors.New("pg error: error connecting to db"),
+			expectedStatusCode: 500,
+			expectedResponse: "{\"httpStatus\":500," +
+				"\"httpCode\":\"internal_server_error\"," +
+				"\"requestId\":\"\",\"errors\":[]}\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			assert := assert.New(t)
+			iceCreamStore := &fakeIceCreamStore{iceCream: test.dbResponse, err: test.dbError}
+			ich := NewIceCreamHandler(iceCreamStore)
+
+			req, err := http.NewRequest("GET", "/url", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rctx := chi.NewRouteContext()
+			rctx.URLParams.Add("ice-cream-name", "chocobar")
+			ctx := context.WithValue(context.Background(), chi.RouteCtxKey, rctx)
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(ich.GetIceCreamData)
+			handler.ServeHTTP(rr, req.WithContext(ctx))
+			assert.Equal(test.expectedResponse, rr.Body.String())
+			assert.Equal(test.expectedStatusCode, rr.Code)
+		})
+	}
+
 }
